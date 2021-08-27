87cea1b550aca8921ba652260c55a7116d4bb625
Alzenas <public@abu-samra.com>
2021-08-27 13:04:41 +0200

    Added test properties for soil layers and wall segments. Some minor amendments to classes.

--- retwall.py
+++ retwall.py
@@ -9,14 +9,82 @@ if __name__ == '__main__':
     c35 = Concrete(structural_code, "C35/45", 35, 25)
     c45 = Concrete(structural_code, "C45/55", 45, 25)
 
-    soil = Soil()
-    soil.phi = 30
+    # Define test soil layers --------------------------------------------
+    geology = []
+    numberOfSoilLayers = 4
 
-    print(soil.ka()[0], soil.ka()[1])
-    print(soil.kp()[0], soil.kp()[1])
-    print(soil.k0()[0], soil.k0()[1])
+    for i in range(0, numberOfSoilLayers):
+        geology.append(Soil())
+        geology[i].name = "Soil Layer {}".format(i)
+        geology[i].top_offset = 0
+        geology[i].isTopLayer = False
 
-    print(soil.phi)
-    print(soil.wall_friction)
+    i = 0
+    geology[i].isTopLayer = True
+    geology[i].top_offset = 1.5
+    geology[i].layer_thickness = 1.0
+    geology[i].inclination = 20.0
+
+    i = 1
+    geology[i].layer_thickness = 2.0
+
+    i = 2
+    geology[i].layer_thickness = 0.90
+
+    i = 3
+    geology[i].layer_thickness = 2.0
+
+    verticalSoilPoints = [0]
+    for i in range(0, numberOfSoilLayers):
+        if (geology[i].isTopLayer):
+            verticalSoilPoints.append(verticalSoilPoints[-1] + geology[i].top_offset)
+        verticalSoilPoints.append(verticalSoilPoints[-1] + geology[i].layer_thickness)
+
+    print(verticalSoilPoints)
+
+    # Enf of Test Soil Layers Definitions ------------------------------------------------
+
+    # Define test walls ------------------------------------------------------------------
+    numberOfSegments = 4  # number of walls
+    wallSegment = []
+    for i in range(0, numberOfSegments):
+        wallSegment.append(Wall(i))
+        wallSegment[i].length = 1.0  # unit length of the wall
+
+    i = 0  # first segment
+    wallSegment[i].name = "Parapet"
+    wallSegment[i].thickness = 150
+    wallSegment[i].height = 1.2
+    wallSegment[i].alignment = Alignment.Left
+
+    i = 1  # second segment
+    wallSegment[i].name = "Top Wall"
+    wallSegment[i].thickness = 250
+    wallSegment[i].height = 2.0
+    wallSegment[i].alignment = Alignment.Left
+
+    i = 2  # third segment
+    wallSegment[i].name = "Lower Wall"
+    wallSegment[i].thickness = 350
+    wallSegment[i].height = 1.5
+    wallSegment[i].alignment = Alignment.Left
+
+    i = 3  # fourth segment (heel)
+    wallSegment[i].name = "Heel"
+    wallSegment[i].thickness = 250
+    wallSegment[i].height = 0.95
+    wallSegment[i].alignment = Alignment.Left
+
+    verticalWallPoints = [0]
+    for i in range(0, numberOfSegments):
+        verticalWallPoints.append(wallSegment[i].height + verticalWallPoints[-1])
+
+    print(verticalWallPoints)
+    # enf of temporary wall definitions --------------------------------------
+
+    # Collect all vertical points into a single, sorted list (use a set, to remove duplicates)
+    verticalPoints = sorted(set(verticalSoilPoints + verticalWallPoints))
+    print("Significant vertical points: {}".format(verticalPoints))
+
 
     pass
--- structural/elements.py
+++ structural/elements.py
@@ -29,6 +29,12 @@ class Shape:
     Sheet = 3
 
 
+class Alignment:
+    Left = "Left"
+    Right = "Right"
+    Centre = "Centre"
+
+
 class WallSection:
     """Cross section class."""
 
@@ -40,14 +46,25 @@ class Wall:
     """Concrete Wall Class. Defines basic properties of a concrete wall."""
 
     def __init__(self, _id):
-        self.ID = _id
-        self.name = []
-        self.thickness = []
-        self.height = []
-        self.length = []
+        self.ID = _id  # Unique identifier
+        self.name = []  # Descriptive name of wall layer
+        self.thickness = []  # thickness of wall segment
+        self.height = []  # height of wall segment
+        self.length = 1.0  # unit length of wall segment
+        self.alignment = Alignment.Left  # wall alignment in relation to the topmost wall segment
 
 
-class Force:
+class UniformLoad:
+    """Uniformly distributed Load Class."""
+
+    def __init__(self):
+        self.x = []  # horiontal location of the start of the UDL, as measured from the wall
+        self.y = []  # vertical location of the load, as measured from a given datum
+        self.magnitude = []  # magnitude of the UDL in
+        self.length = []  # length of the UDL, measured away from the wall
+
+
+class PointForce:
     """Force class. Defines basic properties of force."""
 
     def __init__(self):
@@ -75,20 +92,25 @@ class Displacement:
 
 class Soil:
     def __init__(self,
-                 title='Default Soil',
-                 weight=18,
-                 phi=30):
-        self.title = title
+                 name='Default Soil',
+                 weight=18.0,
+                 phi=30.0):
+        self.name = name
         self.weight = weight
         self.phi = phi
-        self.wall_friction = self.phi * 2 / 3
+        self.wall_friction = round(self.phi * 2 / 3, 1)
+        self.cohesion = 0.0
+        self.layer_thickness = 1.0
+        self.isTopLayer = False
+        self.top_offset = 0.0
+        self.inclination = 0
 
     def ka(self):
         """Calculates active soil pressure coefficient"""
         _angle = radians(self.phi)
         _msg = '"{}": Active pressure coefficient was calculated as {}.'
         _ret = round((1 - sin(_angle)) / (1 + sin(_angle)), 3)
-        _msg = _msg.format(self.title, _ret)
+        _msg = _msg.format(self.name, _ret)
         # return ka and a message
         return [_ret, _msg]
 
@@ -98,7 +120,7 @@ class Soil:
         _msg = '"{}": Passive pressure coefficient was calculated as {}.'
         if self.phi < 90:
             _ret = round((1 + sin(_angle)) / (1 - sin(_angle)), 3)
-            _msg = _msg.format(self.title, _ret)
+            _msg = _msg.format(self.name, _ret)
         else:
             _msg = "Specified angle: {}, but should not be be >= 90".format(self.phi)
             _ret = 999
@@ -111,7 +133,7 @@ class Soil:
         _msg = '"{}": "At rest" pressure coefficient was calculated as {}.'
         _angle = radians(self.phi)
         _ret = round(1 - sin(_angle), 3)
-        _msg = _msg.format(self.title, _ret)
+        _msg = _msg.format(self.name, _ret)
         # return ka and a message
         return [_ret, _msg]
 
